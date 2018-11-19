4f2a3b3cfb07406940ca0c0fab7239e0a7475064
Alejandro Nunez Doniga <anunezdo@alpha10.aulas.gsyc.urjc.es>
2018-11-19 17:53:53 +0100

    Hasta envio RTP funcionando

--- server.py
+++ server.py
@@ -6,12 +6,13 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 
 import socketserver
 import sys
+import os
 
 
 try:
     IP = sys.argv[1]
     PORT = int(sys.argv[2])
-    AUDIO_FILE = sys.argv[3]
+    fichero_audio = sys.argv[3]
 except IndexError:
     sys.exit('Usage: python3 server.py IP port audio_file')
 
@@ -26,18 +27,20 @@ class EchoHandler(socketserver.DatagramRequestHandler):
             method = line.decode('utf-8').split(' ')[0]
             if method not in ['INVITE', 'BYE', 'ACK']:
                 self.wfile.write(b'SIP/2.0 405 Method Not Allowed\r\n')
-                break
             if method == 'INVITE':
                 self.wfile.write(b'SIP/2.0 100 Triying\r\n')
-                self.wfile.write(b'SIP/2.0 180 RingING\r\n')
+                self.wfile.write(b'SIP/2.0 180 Ringing\r\n')
                 self.wfile.write(b'SIP/2.0 200 OK\r\n')
                 print("El cliente nos manda " + line.decode('utf-8'))
             elif method == 'ACK':
-                self.wfile.write(b'RTP\r\n')
-                print("El cliente nos manda " + line.decode('utf-8'))
+                # aEjecutar es un string con lo que se ha de ejecutar en la shell
+                aEjecutar = 'mp32rtp -i 127.0.0.1 -p 23032 < ' + fichero_audio
+                print("Vamos a ejecutar", aEjecutar)
+                os.system(aEjecutar)
             elif method == 'BYE':
                 self.wfile.write(b'SIP/2.0 200 OK\r\n')
                 print("El cliente nos manda " + line.decode('utf-8'))
+            break
 
 
 if __name__ == "__main__":
